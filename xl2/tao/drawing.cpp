6b149c745c10b24b0791995cb299b53ab3e5700a
Christophe de Dinechin <christophe@taodyne.com>
2010-02-28 16:38:35 +0100

    Added a basic "Drawing" class (likely to change over time)

---
+++ xl2/tao/drawing.cpp
@@ -0,0 +1,27 @@
+// ****************************************************************************
+//  drawing.cpp                                                     Tao project
+// ****************************************************************************
+// 
+//   File Description:
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// ****************************************************************************
+// This document is released under the GNU General Public License.
+// See http://www.gnu.org/copyleft/gpl.html and Matthew 25:22 for details
+//  (C) 1992-2010 Christophe de Dinechin <christophe@taodyne.com>
+//  (C) 2010 Taodyne SAS
+// ****************************************************************************
+
+#include "drawing.h"
+
+TAO_BEGIN
+
+TAO_END

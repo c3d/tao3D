7987ac3bbf6e0d312f25158e1b16785481ddebe0
Christophe de Dinechin <christophe@taodyne.com>
2010-03-19 18:10:05 +0100

    Fixed messages for what changed

--- xl2/tao/shapename.cpp
+++ xl2/tao/shapename.cpp
@@ -64,14 +64,14 @@ ShapeName::~ShapeName()
             if (wp)     updateArg(wp,  v.x);
             if (hdp)    updateArg(hdp, v.y);
             if (v.x != 0 || v.y != 0.0)
-                widget->markChanged("Moved shape");
+                widget->markChanged("Resized shape");
         }
         else
         {
             if (xp)     updateArg(xp,  v.x);
             if (yzp)    updateArg(yzp, v.y);
             if (v.x != 0 || v.y != 0.0)
-                widget->markChanged("Resized shape");
+                widget->markChanged("Moved shape");
         }
         box.Normalize();
         widget->drawSelection(box);

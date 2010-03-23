467732d7977d7c593f78ed94f8046dfe10dc6433
Christophe de Dinechin <christophe@taodyne.com>
2010-03-23 11:15:45 +0100

    Avoid asking for confirmation if the library is the default one

--- xl2/tao/application.cpp
+++ xl2/tao/application.cpp
@@ -90,8 +90,6 @@ Application::Application(int & argc, char ** argv)
                                     " Performance may not be optimal."
                                     " Consider updating the OpenGL drivers."));
     }
-
-
 }
 
 
@@ -130,6 +128,8 @@ bool Application::openLibrary(QString path, bool confirm)
             ok = false;
         }
         path = QDir::cleanPath(path);
+        if (path == userDocumentLibraryPath())
+            confirm = false;
 
         if (!ok)
             path = QInputDialog::getText(NULL, tr("Welcome to Tao!"),

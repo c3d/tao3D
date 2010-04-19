778beacff61d5fd09d87d0cbf414e5a0f6dea4f7
Christophe de Dinechin <christophe@taodyne.com>
2010-04-19 14:43:38 +0200

    Fix NULL-deref in IFTRACE at startup

--- xl2/xlr/base.h
+++ xl2/xlr/base.h
@@ -182,7 +182,7 @@ externc void xl_assert_failed(kstring msg, kstring file, uint line);
 
 #ifdef XL_DEBUG
 #  define IFTRACE(x)    if XLTRACE(x)
-#  define XLTRACE(x)    (XL::Options::options->traces.x)
+#  define XLTRACE(x)    (XL::Options::options && XL::Options::options->traces.x)
 #else
 #  define IFTRACE(x)    if(0)
 #  define XLTRACE(x)    0

1252391474e0b87bbb0fd8635071c2ec13cae8cd
Christophe de Dinechin <christophe@taodyne.com>
2010-04-19 10:14:25 +0200

    Replace x ^ 1 with !x, because the selection can have value 2 or 3 or 4...
    When we click on the same item multiple times, we increase the selection count.
    As a result, we cannot assume that the value is 1.

--- xl2/tao/selection.cpp
+++ xl2/tao/selection.cpp
@@ -240,8 +240,7 @@ Activity *Selection::MouseMove(int x, int y, bool active)
             uint size = ptr[0];
             selected = ptr[3];
             if (selected)
-                widget->selection[selected] =
-                        widget->savedSelection[selected] ^ 1;
+                widget->selection[selected] = !widget->savedSelection[selected];
             ptr += 3 + size;
         }
     }

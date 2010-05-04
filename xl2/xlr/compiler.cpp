1dec84dd3ee148b8076e7d632df037a85fe798d3
Christophe de Dinechin <christophe@taodyne.com>
2010-05-04 23:57:05 +0200

    Allow symbolic names that are not evaluated

--- xl2/xlr/compiler.cpp
+++ xl2/xlr/compiler.cpp
@@ -1230,7 +1230,6 @@ Value *CompiledUnit::CallTypeError(Tree_p what)
     Value *ptr = ConstantTree(what); assert(what);
     Value *callVal = code->CreateCall(compiler->xl_type_error, ptr);
     MarkComputed(what, callVal);
-    code->CreateBr(exitbb);
     return callVal;
 }
 

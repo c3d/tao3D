0bb1ead3718c5042178e8657ac33cba0c1d25c9d
Jerome Forissier <jerome@taodyne.com>
2010-05-05 10:45:17 +0200

    Move Windows ldd.exe tool into packaging/win directory

--- xl2/tao/packaging/win/pkg.sh
+++ xl2/tao/packaging/win/pkg.sh
@@ -10,17 +10,25 @@ doo() {
   eval $*
 }
 
-(
+THISDIR=`dirname $0`
+
+#
+# Copy application and dependencies into buildroot/
+#
+
 OBJDIR=../../$MODE
 SRCDIR=../..
+(
+cd $THISDIR
+export PATH=$PATH:.
 rm -rf buildroot
 mkdir buildroot
-doo cp $OBJDIR/tao.exe buildroot/
+doo cp $OBJDIR/Tao.exe buildroot/
 SFILES="builtins.xl xl.syntax xl.stylesheet git.stylesheet"
 for f in $SFILES ; do
     doo cp $SRCDIR/$f buildroot/
 done
-for f in `ldd $OBJDIR/tao.exe | grep -v -i 'windows/system' | grep -v -i 'ntdll.dll' | grep -v -i 'comctl' | sed 's/^.*=> \\(.*\\)(0x.*)$/\\1/' | sed 's@/cygdrive@@'` ; 
+for f in `ldd buildroot/Tao.exe | grep -v -i 'windows/system' | grep -v -i 'ntdll.dll' | grep -v -i 'comctl' | sed 's/^.*=> \\(.*\\)(0x.*)$/\\1/' | sed 's@/cygdrive@@'` ; 
 do 
     doo cp $f buildroot/
 done

c3d/tao3D

commit a8ccd27cd93454f002ac9c19509c23a087fee020

Show load and save progress in status bar

xl2/tao/window.cpp
@@ -993,6 +993,8 @@ bool Window::loadFile(const QString &fileName, bool openProj)
     if (XL::MAIN->options.fileLoad)
             std::cerr << "Opening document: " << +fileName << "\n";
 
+    QString msg = QString(tr("Loading %1 [%2]...")).arg(fileName);
+
     QString docPath = QFileInfo(fileName).canonicalPath();
     if (openProj &&
         !openProject(docPath,
@@ -1001,7 +1003,8 @@ bool Window::loadFile(const QString &fileName, bool openProj)
 
     QApplication::setOverrideCursor(Qt::WaitCursor);
 
-    statusBar()->showMessage(tr("Loading fonts"));
+    statusBar()->showMessage(msg.arg(tr("fonts")));
+    QApplication::processEvents();
     FontFileManager ffm;
     appFontIds = ffm.LoadEmbeddedFonts(fileName);
     if (!appFontIds.empty())
@@ -1009,7 +1012,8 @@ bool Window::loadFile(const QString &fileName, bool openProj)
     foreach (QString e, ffm.errors)
         addError(e);
 
-    statusBar()->showMessage(tr("Loading document"));
+    statusBar()->showMessage(msg.arg(tr("document")));
+    QApplication::processEvents();
     // FIXME: the whole search path stuff is broken when multiple documents
     // are open. There is no way to make "xl:" have a different meaning in
     // two Window instances. And yet it's what we need!
@@ -1026,6 +1030,7 @@ bool Window::loadFile(const QString &fileName, bool openProj)
     {
         // File not found, or parse error
         statusBar()->showMessage(tr("Load error"), 2000);
+        QApplication::processEvents();
         // Try to show source as plain text
         if (!loadFileIntoSourceFileView(fileName, openProj))
             return false;
@@ -1040,6 +1045,7 @@ bool Window::loadFile(const QString &fileName, bool openProj)
         QApplication::restoreOverrideCursor();
         setCurrentFile(fileName);
         statusBar()->showMessage(tr("File loaded"), 2000);
+        QApplication::processEvents();
     }
     isUntitled = false;
     return true;
@@ -1148,6 +1154,9 @@ bool Window::saveFile(const QString &fileName)
         return false;
     }
 
+    statusBar()->showMessage(tr("Saving..."));
+    QApplication::processEvents();
+
     do
     {
         QTextStream out(&file);
@@ -1162,6 +1171,7 @@ bool Window::saveFile(const QString &fileName)
     xlRuntime->LoadFile(fn);
 
     statusBar()->showMessage(tr("File saved"), 2000);
+    QApplication::processEvents();
     updateProgram(fileName);
     isReadOnly = false;
 

commit 7781b749c0d7fb689231bdf2937a5b6f940ff04d

Fix regression with "Save with fonts" feature with multiple page documents (missing fonts)
Regression caused by commit a8ccd27. For some (yet unexplained) reason,
we can't call QApplication::processEvents() in Window::saveFile() just
before writing the file.

Also, factored some code.

xl2/tao/window.cpp
@@ -1004,10 +1004,12 @@ void Window::showMessage(QString message, int timeout)
     {
         QColor gray60(102, 102, 102);
         splashScreen->showMessage(message, Qt::AlignBottom, gray60);
-        QApplication::processEvents();
-        return;
     }
-    statusBar()->showMessage(message, timeout);
+    else
+    {
+        statusBar()->showMessage(message, timeout);
+    }
+    QApplication::processEvents();
 }
 
 
@@ -1030,7 +1032,6 @@ bool Window::loadFile(const QString &fileName, bool openProj)
     QApplication::setOverrideCursor(Qt::WaitCursor);
 
     showMessage(msg.arg(tr("fonts")));
-    QApplication::processEvents();
     FontFileManager ffm;
     appFontIds = ffm.LoadEmbeddedFonts(fileName);
     if (!appFontIds.empty())
@@ -1039,7 +1040,6 @@ bool Window::loadFile(const QString &fileName, bool openProj)
         addError(e);
 
     showMessage(msg.arg(tr("document")));
-    QApplication::processEvents();
     // FIXME: the whole search path stuff is broken when multiple documents
     // are open. There is no way to make "xl:" have a different meaning in
     // two Window instances. And yet it's what we need!
@@ -1056,7 +1056,6 @@ bool Window::loadFile(const QString &fileName, bool openProj)
     {
         // File not found, or parse error
         showMessage(tr("Load error"), 2000);
-        QApplication::processEvents();
         // Try to show source as plain text
         if (!loadFileIntoSourceFileView(fileName, openProj))
             return false;
@@ -1071,7 +1070,6 @@ bool Window::loadFile(const QString &fileName, bool openProj)
         QApplication::restoreOverrideCursor();
         setCurrentFile(fileName);
         showMessage(tr("File loaded"), 2000);
-        QApplication::processEvents();
     }
     isUntitled = false;
     return true;
@@ -1183,7 +1181,9 @@ bool Window::saveFile(const QString &fileName)
     }
 
     statusBar()->showMessage(tr("Saving..."));
-    QApplication::processEvents();
+    // FIXME: can't call processEvent here, or the "Save with fonts..."
+    // function fails to save all the fonts of a multi-page doc
+    // QApplication::processEvents();
 
     do
     {
@@ -1198,8 +1198,7 @@ bool Window::saveFile(const QString &fileName)
     setCurrentFile(fileName);
     xlRuntime->LoadFile(fn);
 
-    statusBar()->showMessage(tr("File saved"), 2000);
-    QApplication::processEvents();
+    showMessage(tr("File saved"), 2000);
     updateProgram(fileName);
     isReadOnly = false;
 
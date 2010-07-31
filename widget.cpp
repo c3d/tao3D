70f26244b6cdd24223c4881952a4d8b76fdf4487
Catherine Burvelle <catherine@taodyne.com>
2010-07-31 09:38:01 +0200

    refs:Feature#242 Documentation is very long : 40% done

--- widget.cpp
+++ widget.cpp
@@ -3502,7 +3502,7 @@ XL::Name_p Widget::enableAnimations(XL::Tree_p self, bool fs)
 
 XL::Name_p Widget::enableStereoscopy(XL::Tree_p self, bool fs)
 // ----------------------------------------------------------------------------
-//   Enable or disable animations
+//   Enable or disable stereoscopie mode
 // ----------------------------------------------------------------------------
 {
     bool oldFs = hasAnimations();

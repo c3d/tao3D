c3d/tao3D

commit 4f99a42faa43888e127f51a7fe64966230a2a8a1

Revert to and Selective undo dialogs: replace history combo box with table view. Closes #295.

tao/repository.h
@@ -88,17 +88,24 @@ public:
     struct Commit
     {
         Commit() {}
-        Commit(QString id, QString msg = ""): id(id), msg(msg) {}
-        Commit(const Commit &o): id(o.id), msg(o.msg) {}
+        Commit(QString id, QString msg = "", QString unixTime = "",
+               QString author = ""): id(id), msg(msg),
+               author(author)
+               { date = QDateTime().fromTime_t(unixTime.toUInt()); }
+        Commit(const Commit &o): id(o.id), msg(o.msg), date(o.date),
+               author(o.author) {}
         Commit& operator = (const Commit &o)
         {
             id = o.id;
             msg = o.msg;
+            date = o.date;
+            author = o.author;
             return *this;
         }
         bool operator == (const Commit &o) const
         {
-            return id == o.id && msg == o.msg;
+            return id == o.id && msg == o.msg &&
+                   date == o.date && author == o.author;
         }
         bool operator != (const Commit &o) const
         {
@@ -107,6 +114,10 @@ public:
         QString toString()
         {
             QString ret = id;
+            if (date.isValid())
+                ret += " " + date.toString();
+            if (!author.isEmpty())
+                ret += " " + author;
             if (!msg.isEmpty())
                 ret += " " + msg;
             return ret;
@@ -114,6 +125,8 @@ public:
 
         QString id;
         QString msg;
+        QDateTime date;
+        QString author;
     };
 
     static struct Commit HeadCommit;

tao/commit_table_model.cpp
@@ -1,0 +1,171 @@
+// ****************************************************************************
+//  commit_table_model.cpp                                        Tao project
+// ****************************************************************************
+//
+//   File Description:
+//
+//    The data model for CommitTableWidget. Basically, it holds a list of
+//    repository commits.
+//
+//
+//
+//
+//
+//
+//
+// ****************************************************************************
+// This document is released under the GNU General Public License.
+// See http://www.gnu.org/copyleft/gpl.html and Matthew 25:22 for details
+//  (C) 2010 Jerome Forissier <jerome@taodyne.com>
+//  (C) 2010 Taodyne SAS
+// ****************************************************************************
+
+#include "commit_table_model.h"
+
+namespace Tao {
+
+CommitTableModel::CommitTableModel(QObject *parent)
+// ----------------------------------------------------------------------------
+//    Create a CommitTableModel
+// ----------------------------------------------------------------------------
+    : QAbstractTableModel(parent)
+{
+}
+
+
+int CommitTableModel::rowCount(const QModelIndex &parent) const
+// ----------------------------------------------------------------------------
+//    Return the number of rows in the model
+// ----------------------------------------------------------------------------
+{
+    (void)parent;
+    return commits.size();
+}
+
+
+int CommitTableModel::columnCount(const QModelIndex &parent) const
+// ----------------------------------------------------------------------------
+//    Return the number of columns in the model
+// ----------------------------------------------------------------------------
+{
+    (void)parent;
+    return 4;
+}
+
+
+QVariant CommitTableModel::data(const QModelIndex &index, int role) const
+// ----------------------------------------------------------------------------
+//    Return the model data for a given element of the table
+// ----------------------------------------------------------------------------
+{
+    if (!index.isValid() ||
+        index.row() >= commits.size() ||
+        index.row() < 0)
+        return QVariant();
+
+    if (role == Qt::DisplayRole)
+    {
+        Repository::Commit commit = commits.at(index.row());
+
+        switch (index.column())
+        {
+        case 0:    return commit.id;
+        case 1:    return commit.date;
+        case 2:    return commit.author;
+        case 3:    return commit.msg;
+        default:   return QVariant();
+        }
+    }
+    return QVariant();
+}
+
+
+QVariant CommitTableModel::headerData(int section, Qt::Orientation orientation,
+                                      int role) const
+// ----------------------------------------------------------------------------
+//    Return the headers of the table
+// ----------------------------------------------------------------------------
+{
+    if (role != Qt::DisplayRole)
+        return QVariant();
+
+    if (orientation == Qt::Horizontal)
+    {
+        switch (section)
+        {
+        case 0:     return tr("ID");
+        case 1:     return tr("Date");
+        case 2:     return tr("Author");
+        case 3:     return tr("Message");
+        default:    return QVariant();
+        }
+    }
+    return QVariant();
+}
+
+
+Repository::Commit CommitTableModel::commit(int row)
+// ----------------------------------------------------------------------------
+//    Return the commit at position 'row' in the commit list
+// ----------------------------------------------------------------------------
+{
+    return commits[row];
+}
+
+
+void CommitTableModel::refresh()
+// ----------------------------------------------------------------------------
+//    Refresh commit list
+// ----------------------------------------------------------------------------
+{
+    if (!repo)
+        return;
+
+    beginResetModel();
+
+    commits.clear();
+    // Put commits in reverse chronological order
+    QList<Repository::Commit> tmp = repo->history(branch);
+    foreach (Repository::Commit commit, tmp)
+        commits.prepend(commit);
+
+    endResetModel();
+}
+
+
+void CommitTableModel::setRepository(Repository *repo)
+// ----------------------------------------------------------------------------
+//    Associate a repository to the model
+// ----------------------------------------------------------------------------
+{
+    if (!repo)
+        return;
+
+    this->repo = repo;
+}
+
+
+void CommitTableModel::setBranch(QString branch)
+// ----------------------------------------------------------------------------
+//    Select a branch
+// ----------------------------------------------------------------------------
+{
+    this->branch = branch;
+}
+
+
+
+bool CommitSortFilterProxyModel::lessThan(const QModelIndex &left,
+                                          const QModelIndex &right) const
+// ----------------------------------------------------------------------------
+//    Compare to cells in the model
+// ----------------------------------------------------------------------------
+{
+    // We want to preserve exactly the history order when sorting by date
+    if (left.column() == 2)
+        return left.row() < right.row();
+
+    return QSortFilterProxyModel::lessThan(left, right);
+}
+
+}

tao/commit_table_model.h
@@ -1,0 +1,76 @@
+#ifndef COMMIT_TABLE_MODEL_H
+#define COMMIT_TABLE_MODEL_H
+
+// ****************************************************************************
+//  commit_table_model.h                                          Tao project
+// ****************************************************************************
+//
+//   File Description:
+//
+//    The data model for CommitTableWidget. Basically, it holds a list of
+//    repository commits.
+//
+//
+//
+//
+//
+//
+//
+// ****************************************************************************
+// This document is released under the GNU General Public License.
+// See http://www.gnu.org/copyleft/gpl.html and Matthew 25:22 for details
+//  (C) 2010 Jerome Forissier <jerome@taodyne.com>
+//  (C) 2010 Taodyne SAS
+// ****************************************************************************
+
+#include "repository.h"
+#include <QAbstractTableModel>
+#include <QModelIndex>
+#include <QVariant>
+#include <QList>
+#include <QSortFilterProxyModel>
+
+namespace Tao {
+
+class CommitTableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+public:
+    CommitTableModel(QObject *parent = 0);
+
+    int      rowCount(const QModelIndex &parent) const;
+    int      columnCount(const QModelIndex &parent) const;
+    QVariant data(const QModelIndex &index, int role) const;
+    QVariant headerData(int section, Qt::Orientation orient, int role) const;
+
+    void     refresh();
+    Repository::Commit commit(int row);
+
+public slots:
+    void    setRepository(Repository *repo);
+    void    setBranch(QString branch);
+
+private:
+    Repository *              repo;
+    QString                   branch;
+    QList<Repository::Commit> commits;
+};
+
+class CommitSortFilterProxyModel : public QSortFilterProxyModel
+// ----------------------------------------------------------------------------
+//    Custom sort/filter model to re-implement sorting by date
+// ----------------------------------------------------------------------------
+{
+    Q_OBJECT
+
+public:
+    CommitSortFilterProxyModel(QObject *parent)
+        : QSortFilterProxyModel(parent) {}
+
+    bool    lessThan(const QModelIndex &left, const QModelIndex &right) const;
+};
+
+}
+
+#endif // COMMIT_TABLE_MODEL_H

tao/commit_table_widget.cpp
@@ -1,0 +1,101 @@
+// ****************************************************************************
+//  commit_table_widget.cpp                                        Tao project
+// ****************************************************************************
+//
+//   File Description:
+//
+//    A class to display a table view showing the commit log for a branch
+//
+//
+//
+//
+//
+//
+//
+//
+// ****************************************************************************
+// This document is released under the GNU General Public License.
+// See http://www.gnu.org/copyleft/gpl.html and Matthew 25:22 for details
+//  (C) 2010 Jerome Forissier <jerome@taodyne.com>
+//  (C) 2010 Taodyne SAS
+// ****************************************************************************
+
+#include "commit_table_widget.h"
+#include "commit_table_model.h"
+
+namespace Tao {
+
+CommitTableWidget::CommitTableWidget(QWidget *parent)
+// ----------------------------------------------------------------------------
+//    Create a CommitTableWidget
+// ----------------------------------------------------------------------------
+    : QTableView(parent)
+{
+    table = new CommitTableModel(this);
+
+    proxyModel = new CommitSortFilterProxyModel(this);
+    proxyModel->setSourceModel(table);
+
+    setModel(proxyModel);
+    setSortingEnabled(true);
+    setSelectionBehavior(QAbstractItemView::SelectRows);
+    horizontalHeader()->setStretchLastSection(true);
+    verticalHeader()->hide();
+    setEditTriggers(QAbstractItemView::NoEditTriggers);
+    setSelectionMode(QAbstractItemView::SingleSelection);
+
+    sortByColumn(1, Qt::DescendingOrder);
+    horizontalHeader()->setSortIndicator(1, Qt::DescendingOrder);
+
+    connect(selectionModel(),
+            SIGNAL(selectionChanged(QItemSelection,QItemSelection)),
+            this, SLOT(onSelectionChanged()));
+}
+
+
+void  CommitTableWidget::refresh()
+// ----------------------------------------------------------------------------
+//    Reload the commit history from the model
+// ----------------------------------------------------------------------------
+{
+    table->refresh();
+}
+
+
+void CommitTableWidget::setRepository(Repository *repo)
+// ----------------------------------------------------------------------------
+//    Set the repository to use as a data source
+// ----------------------------------------------------------------------------
+{
+    table->setRepository(repo);
+}
+
+
+void CommitTableWidget::setBranch(QString branch)
+// ----------------------------------------------------------------------------
+//    Choose the branch to display. Select latest commit.
+// ----------------------------------------------------------------------------
+{
+    table->setBranch(branch);
+    refresh();
+    selectRow(0);
+}
+
+
+void CommitTableWidget::onSelectionChanged()
+// ----------------------------------------------------------------------------
+//    Emit a signal with a Commit structure when a line is selected
+// ----------------------------------------------------------------------------
+{
+    QModelIndexList indexes = selectionModel()->selectedRows();
+    if (indexes.size() > 1)
+        std::cerr << "Warning: Multiple selection not supported\n";
+
+    QModelIndex index = indexes.at(0);
+    int row = proxyModel->mapToSource(index).row();
+    Repository::Commit commit = table->commit(row);
+
+   emit commitSelected(commit);
+}
+
+}

tao/commit_table_widget.h
@@ -1,0 +1,61 @@
+#ifndef COMMIT_TABLE_WIDGET_H
+#define COMMIT_TABLE_WIDGET_H
+
+// ****************************************************************************
+//  commit_table_widget.h                                          Tao project
+// ****************************************************************************
+//
+//   File Description:
+//
+//    A class to display a table view that shows the commit log for a branch
+//
+//
+//
+//
+//
+//
+//
+//
+// ****************************************************************************
+// This document is released under the GNU General Public License.
+// See http://www.gnu.org/copyleft/gpl.html and Matthew 25:22 for details
+//  (C) 2010 Jerome Forissier <jerome@taodyne.com>
+//  (C) 2010 Taodyne SAS
+// ****************************************************************************
+
+#include "repository.h"
+#include <QtGui>
+
+
+namespace Tao {
+
+class CommitTableModel;
+
+class CommitTableWidget : public QTableView
+{
+    Q_OBJECT
+
+public:
+    CommitTableWidget(QWidget *parent = 0);
+
+public:
+    void    refresh();
+
+public slots:
+    void    setRepository(Repository *repo);
+    void    setBranch(QString branch);
+
+signals:
+    void    commitSelected(Repository::Commit commit);
+
+private slots:
+    void    onSelectionChanged();
+
+private:
+    CommitTableModel      * table;
+    QSortFilterProxyModel * proxyModel;
+};
+
+}
+
+#endif // COMMIT_TABLE_WIDGET_H

tao/git_backend.cpp
@@ -994,7 +994,7 @@ QList<GitRepository::Commit> GitRepository::history(QString branch, int max)
 //   If branch == "", the current branch is used
 {
     QStringList args;
-    args << "log" << "--pretty=format:%h:%s";
+    args << "log" << "--pretty=format:%h/%at/%an/%s";
     args << "-n" << QString("%1").arg(max);
     if (!branch.isEmpty())
         args << branch;
@@ -1006,12 +1006,12 @@ QList<GitRepository::Commit> GitRepository::history(QString branch, int max)
     QList<Commit>       result;
     QStringList         log = (+output).split("\n");
     QStringListIterator it(log);
-    QRegExp             rx("([^:]+):(.*)");
+    QRegExp             rx("([^/]+)/([0-9]+)/([^/]+)/(.*)");
 
     while (it.hasNext())
         if (rx.indexIn(it.next()) != -1)
-            result.prepend(Repository::Commit(rx.cap(1), rx.cap(2)));
-
+            result.prepend(Repository::Commit(rx.cap(1), rx.cap(4),
+                                              rx.cap(2), rx.cap(3)));
     return result;
 }
 

tao/history_dialog.cpp
@@ -24,7 +24,7 @@
 #include "history_dialog.h"
 #include "repository.h"
 #include "ui_history_dialog.h"
-#include "commit_selection_combobox.h"
+#include "commit_table_widget.h"
 #include "tao_utf8.h"
 #include <QMessageBox>
 #include <QLineEdit>
@@ -42,20 +42,18 @@ HistoryDialog::HistoryDialog(Repository *repo, QWidget *parent)
     setupUi(this);
 
     connect(branchCombo, SIGNAL(branchSelected(QString)),
-            revCombo, SLOT(setBranch(QString)));
-    connect(revCombo, SIGNAL(commitSelected(Repository::Commit)),
-            this, SLOT(revCombo_commitSelected(Repository::Commit)));
+            tableView, SLOT(setBranch(QString)));
+    connect(tableView, SIGNAL(commitSelected(Repository::Commit)),
+            this, SLOT(tableView_commitSelected(Repository::Commit)));
 
-    revCombo->setMode(CommitSelectionComboBox::CSM_CommitId |
-                      CommitSelectionComboBox::CSM_CommitMsg);
-    revCombo->setRepository(repo);
+    tableView->setRepository(repo);
     branchCombo->setRepository(repo);
 }
 
 
-void HistoryDialog::revCombo_commitSelected(Repository::Commit commit)
+void HistoryDialog::tableView_commitSelected(Repository::Commit commit)
 // ----------------------------------------------------------------------------
-//    Update QLineEdit widget when commit is selected through combo box
+//    Update QLineEdit widget when a commit is selected through table view
 // ----------------------------------------------------------------------------
 {
     revEdit->setText(commit.id);

tao/history_dialog.h
@@ -41,7 +41,7 @@ public:
     HistoryDialog(Repository *repo, QWidget *parent = 0);
 
 private slots:
-    void    revCombo_commitSelected(Repository::Commit commit);
+    void    tableView_commitSelected(Repository::Commit commit);
 
 private:
     void    setRepository(Repository *repo);

commit c823286da798c85b53396ec5a0e84cf0e88a635e

Repository::Commit: do not initialize date if no date is given. Fixes #322.
Fixes a regression introduced by 4f99a42f.

tao/repository.h
@@ -91,7 +91,7 @@ public:
         Commit(QString id, QString msg = "", QString unixTime = "",
                QString author = ""): id(id), msg(msg),
                author(author)
-               { date = QDateTime().fromTime_t(unixTime.toUInt()); }
+               { if (!unixTime.isEmpty()) date = QDateTime().fromTime_t(unixTime.toUInt()); }
         Commit(const Commit &o): id(o.id), msg(o.msg), date(o.date),
                author(o.author) {}
         Commit& operator = (const Commit &o)
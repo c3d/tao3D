e03e4e95eed722a69a59b9848bd8b4bd77d7a9e9
Jerome Forissier <jerome@taodyne.com>
2011-03-02 16:48:47 +0100

    Create page for widget documentation

---
+++ tao/doc/tao_widgets.doxy.h
@@ -0,0 +1,18 @@
+/**
+ * @addtogroup Widgets GUI widgets
+ * @ingroup TaoBuiltins
+ *
+ * Inserts Graphical User Interface widgets (such as: text edit box,
+ * push button, radio button, web browser...).
+ *
+ * The widgets available through Tao primitives are provided by the Qt
+ * library. For instance, @c line_edit creates a QLineEdit object,
+ * @c push_button uses QPushButton, and so on.
+ *
+ * @{
+ */
+
+
+/**
+ * @}
+ */

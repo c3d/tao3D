297adbfd60e6f7e77295d191271f042a77d7a4c5
Jerome Forissier <jerome@taodyne.com>
2011-03-17 18:25:40 +0100

    Documentation: rework main page. Explain document structure and execution model.

--- tao/doc/tao.doxy.h
+++ tao/doc/tao.doxy.h
@@ -3,37 +3,152 @@
  *
  * Welcome to the online documentation for Tao Presentations.
  *
- * @section secShowItYourWay Show it your way
- *
- * You can build your slides to show them exactly as you want.
- * Your presentation is programmable. You code it, using predefined building
- * blocks. This allows you to build flexible taylor-made presentations.
- *
- * @section secImpressYourAudience Impress your audience
- *
- * Your slides live in a dynamic-and-3D world, just like you. Create custom
- * animations, fetch live data to feed your graphs, experience stereoscopic
- * slide shows. Your presentations will be interactive and memorable.
- *
- * @section secShareYourWork Share your work
- *
- * You keep a full history of your documents. You can create versions.
- * You can collaborate with other people on a slide deck, in real time,
- * editing the same document simultaneously. No server required.
- *
  * @section secWhatYouWillFind What is in this manual
  *
- * In this manual, you will find valuable reference information on:
- *   @li the XL language used by Tao to describe documents,
- *   @li the special XL primitives defined by the program to enable writing
- *       presentions,
- *   @li some examples.
+ * In this manual, you will find reference information on:
+ *   - the format and structure of Tao documents,
+ *   - the constructs (@a builtins) that you can use to write documents,
+ *   - the XL language, which is the basis of Tao documents.
+ *
+ * The reference pages also contain some examples.
  *
  * @section secMoreInfo More information
  *
- * Find more information and latest news about Tao Presentations on our web
- * site, http://www.taodyne.com.
+ * You may find more information and the latest news about Tao Presentations on
+ * our web site: http://www.taodyne.com.
  *
+ * @section secGettingStarted Getting started
+ *
+ * The easiest way to learn Tao Presentations is probably to start with
+ * sample documents and change them to your liking. This documentation is
+ * there to help you understand specific aspects of the document description
+ * language as you progress.
+ *
+ * A number of examples are available as @a templates. To create a new Tao
+ * document from a template, click <tt>File > New from Template Chooser...</tt>
+ * and follow the wizard. When done, click <tt>View > Document Source</tt> to
+ * bring up a simple source code editor.
+ * You can edit Tao Presentation documents with any text editor that supports
+ * the UTF-8 encoding. Please make sure you do select this format when saving
+ * your file, or non-ASCII characters may not show up correctly in Tao.
+ *
+ * @note Currently, only the main <tt>.ddd</tt> document file can be edited
+ * with the bundled editor. Composite documents (see below) should be modified
+ * with an external text editor.
+ *
+ * @section secTaoDocuments Tao Presentation documents
+ *
+ * A Tao document is a text file with the <tt>.ddd</tt> extension. The
+ * character encoding is <a href="http://en.wikipedia.org/wiki/UTF-8">UTF-8</a>
+ * which enables the use of international characters anywhere in the document.
+ *
+ * In addition to the main <tt>.ddd</tt> file, a Tao document may be composed
+ * of other files:
+ *  - Secondary files imported into the main file, in order to make the
+ *    document more modular and easier to work with. @n
+ *    These files may be <tt>.ddd</tt> files themselves, as long as they are
+ *    valid Tao documents on their own. For instance, you may want to subdivide
+ *    a long or complex presentation into several shorter ones, and then create
+ *    a main <tt>.ddd</tt> file that will simply import all the others. @n
+ *    When an imported file cannot be opened as a Tao document on its own,
+ *    it is usual to name it with a <tt>.xl</tt> extension to denote that the
+ *    file is XL language source.
+ *  - Pictures. Typically (but not necessarily) stored under the @c images
+ *    subdirectory. Several file formats are supported (PNG, JPEG, and so on;
+ *    refer to the @ref image primitive for details).
+ *  - Fonts. TrueType or OpenType fonts may be stored in the @c fonts
+ *    subdirectory.
+ *
+ * @subsection subsecDocStructure Document structure
+ *
+ * Tao documents are written in the Tao language, which enables to describe the
+ * structure of the document (by creating items such as: pages, slides, text
+ * boxes, rectangles and so on), and at the same time define its dynamic properties
+ * such as how the document changes with time or external events (key press
+ * or mouse position, for instance).
+ *
+ * @subsection secXLRef The XL Language
+ *
+ * The XL programming language is the basis of the Tao document syntax.
+ * Detailed information is available in the XL language reference guide.
+ * However, a deep knowledge of XL is not needed to write Tao documents.
+ *
+ * @subsection secBuiltins Tao Builtins and Primitives
+ *
+ * A @a builtin is any language construct that is not user-defined. For
+ * example, @ref page is a builtin defined by the Tao program.
+ *
+ * Two categories of builtins can be distinguished:
+ *  - @ref TaoBuiltins "Tao builtins" are implemented by the Tao Presentations
+ *    program itself, and are imediately available in a document,
+ *  - @ref Modules "Module builtins" are defined in @a modules, and require
+ *    an @ref import statement before they can be used.
+ *
+ * When a builtin is implemented in native code (C or C++), it is called a
+ * @a primitive.
+ *
+ * @subsection secExecModel Execution and Drawing Model
+ *
+ * In order to display a document, Tao @a executes the document (or parts of
+ * it). The steps are the following:
+ *
+ * <ol>
+ * <li> @b Compile and @b execute the document once. This step creates a
+ *      hierarchy of objects of two kinds: @a Drawings and @a Layouts.
+ *      <ul>
+ *        <li> A Drawing is anything that may be drawn. For example,
+ *             executing the @ref rectangle builtin creates a Drawing object
+ *             of type Rectangle.
+ *        <li> A Layout is a special kind of drawing that may contain other
+ *             Drawings. The following builtins create Layouts: @ref page,
+ *             @ref shape, @ref locally, @ref group, @ref anchor, @ref table,
+ *             @ref table_cell.
+ *      </ul>
+ *      In addition to creating objects, the execution of some primitives
+ *      makes the containing layout sensitive to some external events. @n
+ *      For example, @ref seconds will request re-execution of the
+ *      containing layout one second later, while @ref mouse_x will cause
+ *      its re-execution upon reception of a <em>mouse move</em> event.
+ * <li> @b Draw the Layout and Drawing hierarchy created by the document
+ *      execution. @n
+ *      For exemple, drawing a Rectangle object consists in displaying this
+ *      rectangle on the screen with all the attributes that were selected
+ *      during the the program execution (such as: @ref color or
+ *      @ref line_color).
+ * <li> @b Wait for an external event to occur.
+ * <li> @b Dispatch the event from the top to the bottom of the Layout
+ *      hierarchy. When a Layout is encountered that has subscribed to the
+ *      event, @b re-execute the part of the document that is under that
+ *      layout.
+ * <li> @b Draw the complete Layout and Drawing hierarchy.
+ * </ol>
+ *
+ * The following example shows how events trigger partial execution.
+ *
+ * @code
+writeln "1: minutes = " & text minutes
+locally
+    writeln "2: seconds = " & text seconds
+locally
+    writeln "3: mouse_x = " & text mouse_x
+ * @endcode
+ *
+ * The above code first writes three lines of text, one for each
+ * message. This corresponds to the first, complete execution of the
+ * document. @n
+ * Then, line number 2 shows up every second. Indeed, the @ref seconds
+ * builtin has associated a one-second timer to the Layout object created by
+ * the first @ref locally block, its inner code is therefore re-executed
+ * periodically.
+ * Similarly, line number 3 is printed whenever the mouse is moved. Note
+ * that moving the mouse does not cause line number 2 to be executed again,
+ * and the second changing does not cause line number 3 to be run either. @n
+ * When the minute changes, however, all three lines are displayed again.
+ * This is because the @ref minutes builtin has attached a one-minute timer
+ * to the main Layout object. Since the two @ref locally blocks are nested
+ * inside the main Layout of the document, they get re-executed too.
+ *
+ * @bug [#871] Partial execution not triggered correctly
  */
 
 /**
@@ -46,7 +161,6 @@
  * categories as shown above.
  *
  * @todo Document movie / movie_texture?
- * @todo Document GL shader primitives?
  * @todo Document Git-related primitives (branch/commit chooser commands and
  *       checkout)?
  * @todo Document "error" primitives?
@@ -55,5 +169,4 @@
  * @todo Document the z-order management primitives? (bring_to_front, etc.)
  * @todo Document the group management primitives? (group, group_selection,
  *       ungroup_selection)
- * @todo Document the dimension conversion postfixes? (mm, cm, inch, pt)
  */

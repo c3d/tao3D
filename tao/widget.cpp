efe970e8e655a14895faabbe629eed785ccc8181
Catherine Burvelle <catherine@taodyne.com>
2011-03-30 09:50:27 +0200

    fixes #890 : copy paste update and unusable text_edit

--- tao/widget.cpp
+++ tao/widget.cpp
@@ -69,7 +69,7 @@
 #include "version.h"
 #include "documentation.h"
 #include "formulas.h"
-#include "portability.h"
+#include "text_edit.h"
 #include "xl_source_edit.h"
 #include "tool_window.h"
 #include "context.h"
@@ -174,7 +174,8 @@ Widget::Widget(Window *parent, SourceFile *sf)
       cameraPosition(defaultCameraPosition),
       cameraTarget(0.0, 0.0, 0.0), cameraUpVector(0, 1, 0),
       dragging(false), bAutoHideCursor(false), bShowStatistics(false),
-      renderFramesCanceled(false), inOfflineRendering(false), inDraw(false)
+      renderFramesCanceled(false), inOfflineRendering(false), inDraw(false),
+      editCursor(NULL)
 {
     setObjectName(QString("Widget"));
     // Make sure we don't fill background with crap
@@ -1315,7 +1316,7 @@ void Widget::paste()
             }
             sel->replacement = "";
             sel->replace = true;
-            sel->replacement_tree = portability().fromHTML(mimeData->html());
+            sel->replacement_tree = text_portability().fromHTML(mimeData->html());
 
             updateGL();
             return;
@@ -1338,19 +1339,19 @@ void Widget::paste()
     {
         Tree * t = NULL;
         if (mimeData->hasHtml())
-            t = portability().fromHTML(mimeData->html());
+            t = text_portability().fromHTML(mimeData->html());
         else if (mimeData->hasText())
             t = new XL::Prefix(new XL::Name("text"),
                                new XL::Text(+mimeData->text()));
         else return;
         // Insert a text box with that content at the end of the doc/page.
-        XL::Infix * comma = new XL::Infix(",", new XL::Integer(0LL),
-                                          new XL::Integer(0LL));
-        comma = new XL::Infix(",", comma, new XL::Integer(200));
-        comma = new XL::Infix(",", comma, new XL::Integer(200));
-        comma = new XL::Infix(",", comma,
-                              new XL::Prefix(new Name("do"),
-                                             new XL::Block(t, "I+", "I-")));
+        std::vector<Tree*> arg_list;
+        arg_list.push_back( new XL::Integer(0LL));
+        arg_list.push_back( new XL::Integer(0LL));
+        arg_list.push_back( new XL::Integer(200));
+        arg_list.push_back( new XL::Integer(200));
+        arg_list.push_back( new XL::Block(t, "I+", "I-"));
+        XL::Tree *comma = list2tree(arg_list, ",");
         XL::Prefix * tb = new XL::Prefix(new XL::Name("text_box"),
                                          comma);
         tb = new XL::Prefix(new XL::Name("shape"),
@@ -3035,7 +3036,7 @@ bool Widget::markChanged(text reason)
 {
     if (sourceChanged())
         return false;
-    
+
     Repository *repo = repository();
     if (repo)
         repo->markChanged(reason);
@@ -3653,6 +3654,7 @@ bool Widget::focused(Layout *layout)
 //   Test if the current shape is selected
 // ----------------------------------------------------------------------------
 {
+//    std::cerr << "layout id " << layout->id << " FocusId " <<  focusId << std::endl; // CaB
     return layout->id == focusId;
 }
 
@@ -6582,6 +6584,80 @@ bool Widget::addControlBox(Real *x, Real *y, Real *z,
 //
 // ============================================================================
 
+Tree_p  Widget::textEdit(Context *context, Tree_p self,
+                        Real_p x, Real_p y, Real_p w, Real_p h, Tree_p prog)
+// ----------------------------------------------------------------------------
+//   Create a new text edit widget and render text in it
+// ----------------------------------------------------------------------------
+{
+    XL::Save<Layout *> saveLayout(layout, layout->AddChild(layout->id));
+    Tree * result = textEditTexture(context, self, w, h, prog);
+    TextEditSurface *surface = prog->GetInfo<TextEditSurface>();
+    layout->Add(new ClickThroughRectangle(Box(x-w/2, y-h/2, w, h), surface));
+    if (currentShape)
+        layout->Add(new WidgetManipulator(currentShape, x, y, w, h, surface));
+    return result;
+}
+
+
+Tree_p  Widget::textEditTexture(Context *context, Tree_p self,
+                                double w, double h, Tree_p prog)
+// ----------------------------------------------------------------------------
+//   Create a new text edit widget and render text in it
+// ----------------------------------------------------------------------------
+{
+    if (w < 16) w = 16;
+    if (h < 16) h = 16;
+    refreshOn(QEvent::MouseMove);
+
+    // Update document with prog
+    editCursor = new QTextCursor(new QTextDocument(""));
+    Context *currentContext = context;
+    ADJUST_CONTEXT_FOR_INTERPRETER(context);
+    Tree *result = currentContext->Evaluate(prog);
+
+    // Get or build the current frame if we don't have one
+    TextEditSurface *surface = prog->GetInfo<TextEditSurface>();
+    if (!surface)
+    {
+        surface = new TextEditSurface(editCursor->document()->clone(),
+                                      prog->AsBlock(), this);
+        prog->SetInfo<TextEditSurface> (surface);
+    }
+
+    // Resize to requested size, and bind texture
+    surface->resize(w,h);
+    GLuint tex = surface->bind(editCursor->document()->clone());
+    layout->Add(new FillTexture(tex));
+    layout->hasAttributes = true;
+    delete editCursor;
+    editCursor = NULL;
+
+    return result;
+
+}
+
+
+void Widget::updateCursor(Text_p t)
+// ----------------------------------------------------------------------------
+//   Update the cursor with the current layout info and text.
+// ----------------------------------------------------------------------------
+{
+    if (! editCursor) return;
+
+    QTextBlockFormat copyBF = editCursor->blockFormat();
+    QTextCharFormat copyCF = editCursor->charFormat();
+    // 1- check if paragraph format has changed
+    if (modifyBlockFormat(copyBF, layout))
+        editCursor->insertBlock(copyBF);
+
+    // 2- check if character format has changed
+    if (modifyCharFormat(copyCF, layout))
+        editCursor->insertText(+t->value, copyCF);
+    else
+        editCursor->insertText(+t->value);
+}
+
 Tree_p  Widget::textBox(Context *context, Tree_p self,
                         Real_p x, Real_p y, Real_p w, Real_p h, Tree_p prog)
 // ----------------------------------------------------------------------------
@@ -6651,8 +6727,11 @@ Tree_p Widget::textSpan(Tree_p self, Text_p contents)
 {
     if (path)
         TextSpan(contents).Draw(*path, layout);
+    else if (editCursor)
+        updateCursor(contents);
     else
         layout->Add(new TextSpan(contents));
+
     return XL::xl_true;
 }
 
@@ -7322,6 +7401,7 @@ Tree_p Widget::frameTexture(Context *context, Tree_p self,
     Tree_p result = XL::xl_false;
     if (w < 16) w = 16;
     if (h < 16) h = 16;
+    refreshOn(QEvent::MouseMove);
 
     // Get or build the current frame if we don't have one
     MultiFrameInfo<uint> *multiframe = self->GetInfo< MultiFrameInfo<uint> >();
@@ -8282,7 +8362,6 @@ Tree_p Widget::groupBoxTexture(Tree_p self, double w, double h, Text_p lbl)
 {
     if (w < 16) w = 16;
     if (h < 16) h = 16;
-
 
     // Get or build the current frame if we don't have one
     GroupBoxSurface *surface = self->GetInfo<GroupBoxSurface>();
@@ -9548,7 +9627,7 @@ static void generateRewriteDoc(Widget *widget, XL::Rewrite_p rewrite, text &com)
         Text_p t2 = widget->generateDoc(rewrite->to, rewrite->to);
         com += t2->value;
     }
-    
+
     XL::rewrite_table &rewrites = rewrite->hash;
     XL::rewrite_table::iterator i;
     for (i = rewrites.begin(); i != rewrites.end(); i++)
@@ -9578,7 +9657,7 @@ Text_p Widget::generateAllDoc(Tree_p self, text filename)
 
     // Documentation from the primitives files (*.tbl)
     for (XL::Context *globals = xlr->context; globals; globals = globals->scope)
-    {    
+    {
         XL::rewrite_table &rewrites = globals->rewrites;
         XL::rewrite_table::iterator i;
         for (i = rewrites.begin(); i != rewrites.end(); i++)
--- tao/widget.h
+++ tao/widget.h
@@ -571,6 +571,11 @@ public:
                          Real_p w,Real_p h, text_p s);
     Tree_p      lineEditTexture(Tree_p self, double x, double y, Text_p s);
 
+    Tree_p      textEdit(Context *context, Tree_p self,
+                         Real_p x, Real_p y, Real_p w, Real_p h, Tree_p prog);
+    Tree_p      textEditTexture(Context *context, Tree_p self,
+                                double w, double h, Tree_p prog);
+
     Tree_p      abstractButton(Tree_p self, Text_p name,
                                Real_p x, Real_p y, Real_p w, Real_p h);
     Tree_p      pushButton(Tree_p self, Real_p x, Real_p y, Real_p w, Real_p h,
@@ -852,6 +857,9 @@ private:
     double                trueCurrentTime();
     void                  setCurrentTime();
     bool inDraw;
+
+    QTextCursor          * editCursor;
+    void                  updateCursor(Text_p t);
 };
 
 

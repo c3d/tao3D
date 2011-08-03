7f196cfdc2e8e79213f74f0c94987817b53e029c
Jerome Forissier <jerome@taodyne.com>
2011-08-03 10:12:11 +0200

    Add HTML output option to parse_log.sh + minor fixes.

--- parse_log.sh
+++ parse_log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 usage(){
-    echo "Usage: $0 [-s] <tag1> <tag2>"
+    echo "Usage: $0 [options...] <tag1> <tag2>"
     echo ""
     echo "Use 'git log' to extract tracker IDs between two tags/commits."
     echo ""
@@ -14,6 +14,7 @@ usage(){
     echo "  -s      Split bug fixes and features in two separate lists. Implies -f." 
     echo "  -q      Quiet: just list(s) of numbers (no text)." 
     echo "  -a      Sort IDs in ascending order (default is descending order)." 
+    echo "  -html   Generate HTML output (link IDs to Redmine)."
     exit 1
 }
 
@@ -27,16 +28,17 @@ SHOW_ALL=1
 while [ "$1" ] ; do
   case "$1" in
     -f) SHOW_ALL="";;
-    -s) SPLIT=1;;
+    -s) SPLIT=1;SHOW_ALL="";;
     -h) HASH="";;
     -q) QUIET=1;;
     -a) REVERT="";;
+    -html) HTML="1";;
     *)
       if [ -z "$TAG1" ] ; then
         TAG1="$1"
       else
         if [ -z "$TAG2" ] ; then
-          TAG2="$2"
+          TAG2="$1"
         else
           usage
         fi
@@ -45,26 +47,48 @@ while [ "$1" ] ; do
   esac
   shift
 done
+
+SPACE=" "
+[ "$HTML" ] && SPACE="&nbsp;"
+NL="\n"
+[ "$HTML" ] && NL="<br>"
+
+
 FIXED=`git log --pretty=oneline $TAG1..$TAG2 | grep -e '[Ff]ixes #' | sed 's/.*[Ff]ixes #\([0-9][0-9]*\).*/\1/' | sort -n$REVERT | uniq`
 CLOSED=`git log --pretty=oneline $TAG1..$TAG2 | grep -e '[Cc]loses #' | sed 's/.*[Cc]loses #\([0-9][0-9]*\).*/\1/' | sort -n$REVERT | uniq`
 FIXED_AND_CLOSED=`for i in $FIXED $CLOSED ; do echo $i ; done | sort -n$REVERT`
 ALL=`git log --pretty=oneline $TAG1..$TAG2 | awk '{ split($0, array) ; for (item in array) { if (match($item, "#[0-9][0-9]+")) { print substr($item, RSTART+1, RLENGTH-1) } } }' | sort -n$REVERT | uniq`
-[ "$HASH" ] && FIXED=`for i in $FIXED ; do echo \#$i ; done`
-[ "$HASH" ] && CLOSED=`for i in $CLOSED ; do echo \#$i ; done`
-[ "$HASH" ] && FIXED_AND_CLOSED=`for i in $FIXED_AND_CLOSED ; do echo \#$i ; done`
-[ "$HASH" ] && ALL=`for i in $ALL ; do echo \#$i ; done`
 
+print_ids() {
+  eval IDS='$'$1
+  for i in $IDS ; do
+    [ "$HTML" ] && printf "<a href=\"https://nano.taodyne.com/redmine/issues/$i\">"
+    [ "$HASH" ] && printf "#"
+    printf $i
+    [ "$HTML" ] && printf "</a>"
+    printf "$SPACE"
+  done
+}
+
+ALL=`print_ids ALL`
+FIXED=`print_ids FIXED`
+CLOSED=`print_ids CLOSED`
+FIXED_AND_CLOSED=`print_ids FIXED_AND_CLOSED`
+
+[ "$HTML" ] && echo "<html><body>"
+[ "$QUIET" ] || printf "$TAG1 .. $TAG2$NL"
 if [ "$SHOW_ALL" ] ; then
-    [ "$QUIET" ] || echo "All tracker IDs:"
-    echo $ALL 
+    [ "$QUIET" ] || printf "All tracker IDs: "
+    printf "$ALL""$NL" 
 else
   if [ "$SPLIT" ] ; then
-    [ "$QUIET" ] || echo "(Bugs) fixed:"
-    echo $FIXED
-    [ "$QUIET" ] || echo "(Feats) closed:"
-    echo $CLOSED
+    [ "$QUIET" ] || printf "(Bugs) fixed: "
+    printf "$FIXED""$NL"
+    [ "$QUIET" ] || printf "(Feats) closed: "
+    printf "$CLOSED""$NL"
   else
-    [ "$QUIET" ] || echo "Bugs and fetures: "
-    echo $FIXED_AND_CLOSED 
+    [ "$QUIET" ] || printf "Bugs and fetures: "
+    printf "$FIXED_AND_CLOSED""$NL"
   fi
 fi
+[ "$HTML" ] && echo "</body></html>"

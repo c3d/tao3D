f5e542f1539a8af7d846f0219e08d949c607a991
Soulisse Baptiste <baptiste.soulisse@taodyne.com>
2011-12-12 14:16:11 +0100

    Optimize visibility attribute. Closes #1690.

--- tao/shapes.cpp
+++ tao/shapes.cpp
@@ -183,7 +183,7 @@ bool Shape::setFillColor(Layout *where)
     {
         Color &color = where->fillColor;
         scale v = where->visibility * color.alpha;
-        if (v > 0.0)
+        if (v >= 0.01)
         {
             if (!where->hasMaterial)
                 glColor4f(color.red, color.green, color.blue, v);
@@ -206,7 +206,7 @@ bool Shape::setLineColor(Layout *where)
         Color &color = where->lineColor;
         scale width = where->lineWidth;
         scale v = where->visibility * color.alpha;
-        if (v > 0.0 && width > 0.0)
+        if (v >= 0.01 && width > 0.0)
         {
             if (!where->hasMaterial)
                 glColor4f(color.red, color.green, color.blue, v);

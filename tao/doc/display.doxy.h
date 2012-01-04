dea240cba361eb319d06bb058e96926746261a57
Jerome Forissier <jerome@taodyne.com>
2012-01-04 01:15:38 +0100

    French translation of display.doxy.h.

--- tao/doc/display.doxy.h
+++ tao/doc/display.doxy.h
@@ -1,5 +1,6 @@
 /**
- * @defgroup Display Display and stereoscopy controls 
+ * @~english
+ * @defgroup Display Display and stereoscopy controls
  * @ingroup TaoBuiltins
  *
  * Select display mode and stereoscopic 3D settings.
@@ -10,7 +11,7 @@
  * chances are that it can be adequately used with one of the display
  * modes listed on this page.
  * 
- * Here is an illustration of the modes currently supported by Tap
+ * Here is an illustration of the modes currently supported by Tao
  * Presentations (see @ref display_mode): 
  * @image html 2D_small.png "Default (\"default\",  \"2D\")"
  * @image html anaglyph_small.png "Red/blue anaglyph (\"anaglyph\")"
@@ -25,10 +26,40 @@
  * @image html vframepacking_small.png "vertical frame packing (\"vframepacking\")"
  * @image html alioscopy_small.png "Alioscopy® (\"alioscopy\")"
  * @image html alioscopy_detail.png "Alioscopy® (detail)"
+ *
+ * @~french
+ * @defgroup Display Affichage et contrôle de stéréoscopie
+ * @ingroup TaoBuiltins
+ *
+ * Permet de sélectionner le mode d'affichage et les paramètres de 3D
+ * stéréoscopique.
+ * Tao Presentations peut tirer parti de différentes technologies pour afficher
+ * du contenu 3D. En fonction du matériel dont vous disposez, vous pouvez
+ * sélectionner l'un des modes d'affichage disponible dans l'application.
+ * Si vous avez un écran ou un projecteur capable d'afficher en 3D, il y
+ * a de fortes chances que Tao Presentations vous permette de l'exploiter au
+ * mieux.
+ *
+ * Voici une illustration des modes actuellement supportés par Tao
+ * Presentations (cf. @ref display_mode):
+ * @image html 2D_small.png "Défaut (\"default\",  \"2D\")"
+ * @image html anaglyph_small.png "Anaglyph rouge/bleu (\"anaglyph\")"
+ * @image html anaglyph_detail.png "Anaglyph rouge/bleu (détail)"
+ * @image html hsplitstereo_small.png "Côte-à-côte (\"hsplitstereo\", \"sidebysidestereo\")"
+ * @image html vsplitstereo_small.png "Dessus/dessous (\"vsplitstereo\",  \"overunderstereo\")"
+ * @image html hintstereo_small.png "Entrelacé (\"hintstereo\")"
+ * @image html hintstereo_detail.png "Entrelacé (détail)"
+ * @image html checkerboard_small.png "Damier (\"checkerboard\")"
+ * @image html checkerboard_detail.png "Damier (détail)"
+ * @image html hframepacking_small.png "Frame packing horizontal (\"hframepacking\")"
+ * @image html vframepacking_small.png "Frame packing vertical (\"vframepacking\")"
+ * @image html alioscopy_small.png "Alioscopy® (\"alioscopy\")"
+ * @image html alioscopy_detail.png "Alioscopy® (détail)"
  * @{
  */
 
 /**
+ * @~english
  * Controls stereoscopy.
  * When @p mode is @c false, stereoscopic output is disabled, and
  * the 2D output mode is selected.
@@ -36,10 +67,20 @@
  * is enabled, if supported by the hardware. If this mode is not
  * supported, the primitive does nothing.
  * @param mode is @c true or @c false.
+ *
+ * @~french
+ * Active ou désactive la stéréoscopie.
+ * Lorsque @p mode est @c false, l'affichage stéréoscopique est désactivé,
+ * et le mode 2D est actif.
+ * Lorsque @p mode est @c true, l'affichage stéréoscopique quad-buffer
+ * est activé, si le matériel le supporte. Si ce n'est pas le cas, il ne se
+ * passe rien.
+ * @param mode est @c true ou @c false.
  */
 enable_stereoscopy(mode:name);
 
 /**
+ * @~english
  * Select the display mode.
  * @param mode The name of the display mode. Currently supported
  * modes are:
@@ -74,35 +115,87 @@ enable_stereoscopy(mode:name);
  * valid mode strings for your configuration by running Tao Presentations
  * with the @c -tdisplaymode command-line parameter. Look for output
  * similar to:
+ * @~french
+ * Selectionne le mode d'affichage.
+ * @param mode Le nom du mode d'affichage. Les valeurs possibles sont :
+ * - "default", "2D" : affichage en deux dimensions.
+ * - "quadstereo" : 3D stéréoscopique basé sur le quad-buffer OpenGL.
+ *   Le format de sortie exact dépend du pilote OpenGL et de ses réglages.
+ * - "anaglyph" : 3D stéréoscopique sur écran classique avec des
+ *   lunettes rouge et bleu.
+ * - "hsplitstereo", "sidebysidestereo" : 3D stéréoscopique en mode de
+ *   partage horizontal. La moitié gauche de l'écran est pour l'œil gauche,
+ *   la moitié droite et pour l'œil droit. Chaque moitié représente la scène
+ *   complète et est compressée horizontalement. Par exemple, lorsque la
+ *   résolution d'affichage est 1920x1080, on obtient deux images de 960x1080
+ *   côte-à-côte. Ce format convient en général à la plupart des téléviseurs
+ *   3D grand public.
+ * - "vsplitstereo", "overunderstereo" : 3D stéréoscopique en mode de
+ *   partage vertical. Similaire au mode partage horizontal (ci-dessus),
+ *   mais verticalement.
+ * - "hintstereo" : 3D stéréoscopique en mode entrelacé. Certains ordinateurs
+ *   portables avec lunettes passives demandent ce format.
+ * - "checkerboard" : 3D stéréoscopique en mode damier.
+ * - "hframepacking" : 3D stéréoscopique en mode frame packing horizontal.
+ *   Ce mode est similaire au partage horizontal ("hsplitstereo"), mais sans
+ *   compression horizontale.
+ * - "vframepacking" : 3D stéréoscopique en mode frame packing vertical.
+ *   Ce mode est similaire au partage vertical ("vsplitstereo"), mais sans
+ *   compression verticale.
+ * - "alioscopy": un encodage adapté aux
+ *   <a href="http://www.alioscopy.com">écrans 3D Alioscopy® sans lunettes</a>.
+ * @note Les modes d'affichage sont implémentés sous forme de modules. Certains
+ * peuvent ne pas être disponibles, selon votre version de Tao Presentations.
+ * Vous pouvez obtenir une liste de tous les modes valides dans votre
+ * configuration en démarrant Tao Presentations avec l'option
+ * @c -tdisplaymode :
+ * @~
  * @code
 [Display Driver] Registering display function: checkerboard@0x1b4148c0
 [Display Driver] Registering display function: hsplitstereo@0x1c76c110
 [Display Driver] Registering alias: sidebysidestereo => hsplitstereo@0x1c76c110
  * @endcode
+ * @~english
  * Display modes can also be selected with the command chooser (Escape
  * key), or with the View/Display mode menu. 
+ * @~french
+ * Les modes d'affichage peuvent également être sélectionnés par le menu
+ * interactif (touche Échap), ou par le menu Affichage/Mode d'affichage.
  */
 display_mode(mode:text);
 
 /**
+ * @~english
  * The name of the current display mode.
+ * @~french
+ * Le nom du mode d'affichage actuel.
  */
 text display_mode();
 
 /**
+ * @~english
  * Test if a display mode is available.
+ * @~french
+ * Teste si un mode d'affichage est disponible.
  * @see display_mode, display_mode(mode:text)
  */
 boolean has_display_mode(mode:text);
 
 /**
+ * @~english
  * Get eye distance.
  * @return [real] eye distance.
+ * @~french
+ * Renvoie la distance inter-occulaire.
+ * @return [réel] la distance inter-occulaire.
  */
-eye_distance();
+real eye_distance();
 
 /**
+ * @~english
  * Set eye distance.
+ * @~french
+ * Définit la distance inter-occulaire.
  */
 eye_distance(e:real);
 

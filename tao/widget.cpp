44827cdf76f37a36f339e5d0d99a19caf1f3dcf1
Jerome Forissier <jerome@taodyne.com>
2012-05-25 15:01:25 +0200

    Make implementation of Widget::image similar to Widget::fillTexture. Refs #2096.

--- tao/widget.cpp
+++ tao/widget.cpp
@@ -6899,6 +6899,8 @@ Integer* Widget::image(Context *context,
     layout->currentTexture.width  = rinfo->width;
     layout->currentTexture.height = rinfo->height;
     layout->currentTexture.type   = GL_TEXTURE_2D;
+    layout->currentTexture.minFilt = TaoApp->tex2DMinFilter;
+    layout->currentTexture.magFilt = TaoApp->tex2DMagFilter;
 
     uint texId   = layout->currentTexture.id;
 
@@ -6907,10 +6909,7 @@ Integer* Widget::image(Context *context,
     double w = w0 * sx;
     double h = h0 * sy;
 
-    FillTexture * ft = new FillTexture(texId, GL_TEXTURE_2D, true);
-    ft->minFilt = TaoApp->tex2DMinFilter;
-    ft->magFilt = TaoApp->tex2DMagFilter;
-    layout->Add(ft);
+    layout->Add(new FillTexture(texId, GL_TEXTURE_2D, true));
     layout->hasAttributes = true;
 
     Rectangle shape(Box(x-w/2, y-h/2, w, h));

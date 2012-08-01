69a4eaa59d75b00eb976e00afb886caf5b711538
Catherine Burvelle <catherine@taodyne.com>
2012-08-01 14:36:04 +0200

    Adding dip_null protocol. Refs #2314

---
+++ modules/dip_null/doc/dip_null.doxy.h
@@ -0,0 +1,23 @@
+/**
+ * @~english
+ * @taomoduledescription{DIP_null, DIP no data protocol}
+ *
+ * Module for DIP interaction that does not use data.
+ *
+ * @endtaomoduledescription{DIP_null}
+ *
+ * @~french
+ * @taomoduledescription{DIP_null, Protocol DIP sans donnée.}
+ *
+ * Module pour interaction sans donnée avec le DIP. 
+ *
+ * @endtaomoduledescription{DIP_null}
+ *
+ * @~
+ * @{
+ */
+
+
+/**
+ * @}
+ */

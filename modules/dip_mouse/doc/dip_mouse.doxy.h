acc32e88f99ee4c25fc64dbd725587a0d1ad8650
Catherine Burvelle <catherine@taodyne.com>
2012-08-02 10:07:20 +0200

    Adding dip_mouse modules. Refs #2318

---
+++ modules/dip_mouse/doc/dip_mouse.doxy.h
@@ -0,0 +1,23 @@
+/**
+ * @~english
+ * @taomoduledescription{DipMouse, DIP protocol for mouse}
+ *
+ * Protocol for mouse data exchange with the DIP
+ *
+ * @endtaomoduledescription{DipMouse}
+ *
+ * @~french
+ * @taomoduledescription{DipMouse, Protocol du DIP pour la souris}
+ *
+ * Protocol d'interaction avec le DIP de données d'une souris. 
+ *
+ * @endtaomoduledescription{DipMouse}
+ *
+ * @~
+ * @{
+ */
+
+
+/**
+ * @}
+ */

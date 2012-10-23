41b2b9f0cafe4ea2b7e4de5ea7b75e3bbab14e1e
Christophe de Dinechin <christophe@taodyne.com>
2012-10-23 02:45:54 +0200

    Hide worst case of slow down on mouse move, fixes #2563
    We are really hiding the symptoms, by not calling the render callback when in
    a mouse move event. In reality, the underlying issue with driver
    performance during GL_SELECT operations is still there. It can be seen
    with the selection rectangle for example.

--- tao/widget.h
+++ tao/widget.h
@@ -320,6 +320,8 @@ public:
     Layout *    drawTree(Layout *where, Context *context, Tree_p code);
     void        drawCall(Layout *, XL::XLCall &call, uint id=0);
     bool        mouseTracking() { return doMouseTracking; }
+    bool        inMouseMove()   { return w_event &&
+                                  w_event->type() == QEvent::MouseMove; }
 
     template<class Activity>
     Activity *  active();
@@ -941,11 +943,11 @@ private:
     GroupInfo   *         currentGroup;
     GlyphCache            glyphCache;
     FontFileManager *     fontFileMgr;
+    std::vector<StereoIdentTexture>
+                          stereoIdentPatterns;
     bool                  drawAllPages;
     bool                  animated;
     bool                  blanked;
-    std::vector<StereoIdentTexture>
-                          stereoIdentPatterns;
     bool                  stereoIdent;
     bool                  selectionRectangleEnabled;
     bool                  doMouseTracking;

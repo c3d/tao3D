3ac1141163402721bc3584789fda472a589a77fd
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2013-01-21 15:14:19 +0100

    Remove license check of network access module in texture cache.

--- tao/texture_cache.cpp
+++ tao/texture_cache.cpp
@@ -589,7 +589,6 @@ bool CachedTexture::load()
     {
         if (networkReply == NULL)
         {
-            Licenses::CheckImpressOrLicense("NetworkAccess 1.003");
             QUrl url(path);
             QNetworkRequest req(url);
             networkReply = cache.network.get(req);

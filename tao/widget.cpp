7788b88f43a98137c168f4b80ee3eb2459805280
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2013-03-07 16:17:22 +0100

    Assure we reset shader before drawing selection. Closes #2791.

--- tao/widget.cpp
+++ tao/widget.cpp
@@ -4760,6 +4760,7 @@ static inline void resetLayout(Layout *where)
         where->fillColor = Color(0,1,0,0.8);
         where->fillTextures.clear();
         where->previousTextures.clear();
+        where->programId = 0;
     }
 }
 

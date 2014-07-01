18647d2d66f0f663ba34761f806a130615f4e2cb
Christophe de Dinechin <christophe@taodyne.com>
2014-07-01 13:39:53 +0200

    Upload build results to server with scp, closes #3481 @45m

--- build.sh
+++ build.sh
@@ -7,10 +7,29 @@ git fetch --tags
 
 # Create a clean build each time we have a "true" release candidate
 # Other releases have a git hash beginning with g, like 1.50-1-ge0db3af
-(tao/updaterev.sh -n | grep g) || make distclean
+REV="$(tao/updaterev.sh -n)"
+(echo $REV | grep g) || make distclean
 
 # Configure with the Jenkins input parameters
 ./configure -$BUILD_OPT $BUILD_VERSION VLC=$VLCPATH 
 
-# Build it
-make kit
+upload() {
+    if [ "$BUILD_OPT" == "release" -a "$BUILD_VERSION" != "internal" ]; then
+        UNAME=""
+        ( echo $REV | grep g) || UNAME=`uname`
+        case "$UNAME" in
+            Darwin)
+                scp packaging/macosx/Tao*.dmg www.taodyne.com:downloads/
+                ;;
+            MINGW*)
+                scp packaging/win/Tao*.exe www.taodyne.com:downloads/
+                ;;
+            Linux*)
+                scp packaging/linux/Tao*.deb packaging/linux/Tao*.tar.bz2 www.taodyne.com:downloads/
+                ;;
+        esac
+    fi
+}
+
+# Build it and upload on server
+make kit && upload
